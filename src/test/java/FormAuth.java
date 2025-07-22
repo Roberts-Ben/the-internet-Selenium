572e732e995197fbba04f968b53997f9b1a9a381
Ben Roberts <roberts.b93@gmail.com>
2025-07-22 17:55:45 +0100

    More tests added
    - File Upload
    - Form Authentication
    - Geolocation

---
+++ src/test/java/FormAuth.java
@@ -0,0 +1,127 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FormAuth
+{
+    private static final Log log = LogFactory.getLog(FormAuth.class);
+    WebDriver driver;
+    WebDriverWait wait;
+
+    String username = "tomsmith";
+    String password = "SuperSecretPassword!";
+    String invalidUsername = "test";
+    String invalidPassword = "pass";
+
+    @Before
+    public void Setup() throws Exception
+    {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/login", URL);
+    }
+
+    @Test
+    public void verifyValidAuth()
+    {
+        WebElement loginButton = driver.findElement(By.className("fa-sign-in"));
+
+        InputCredentials(true);
+
+        loginButton.click();
+
+        CheckAlert("Success");
+    }
+
+    @Test
+    public void verifyInvalidAuth()
+    {
+        WebElement loginButton = driver.findElement(By.className("fa-sign-in"));
+
+        InputCredentials(false);
+
+        loginButton.click();
+
+        CheckAlert("Invalid");
+    }
+
+    @Test
+    public void verifyLogout()
+    {
+        WebElement loginButton = driver.findElement(By.className("fa-sign-in"));
+
+        InputCredentials(true);
+
+        loginButton.click();
+
+        WebElement logoutButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[href*='/logout']")));
+
+        logoutButton.click();
+
+        CheckAlert("Logout");
+    }
+
+    @Test
+    public void verifyUnableToBypass()
+    {
+        driver.get("https://the-internet.herokuapp.com/secure");
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/login", URL);
+
+        CheckAlert("Bypass");
+    }
+
+    private void InputCredentials(boolean validCredentials)
+    {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+
+        usernameField.sendKeys(validCredentials ? username : invalidUsername);
+        passwordField.sendKeys(validCredentials ? password : invalidPassword);
+    }
+
+    private void CheckAlert(String alertType)
+    {
+        WebElement alert = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div#flash")));
+        String alertText = alert.getText();
+
+        assertTrue(alert.isDisplayed());
+        switch (alertType)
+        {
+            case "Success":
+                assertTrue(alertText.contains("You logged into a secure area!"));
+                break;
+            case "Invalid":
+                assertTrue(alertText.contains("Your username is invalid!"));
+                break;
+            case "Bypass":
+                assertTrue(alertText.contains("You must login to view the secure area!"));
+                break;
+            case "Logout":
+                assertTrue(alertText.contains("You logged out of the secure area!"));
+            default:
+                break;
+        }
+    }
+
+    @After
+    public void Teardown()
+    {
+        driver.quit();
+    }
+}
---
+++ src/test/java/Geolocation.java
@@ -0,0 +1,57 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Geolocation
+{
+    WebDriver driver;
+    WebDriverWait wait;
+
+    String mapsBaseURL = "http://maps.google.com/";
+
+    @Before
+    public void Setup() throws Exception
+    {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/geolocation");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/geolocation", URL);
+    }
+
+    @Test
+    public void verifyLocation()
+    {
+        WebElement locationButton = driver.findElement(By.xpath("//*[@id='content']/div/button"));
+
+        locationButton.click();
+
+        WebElement latValue = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("lat-value")));
+        WebElement longValue = driver.findElement(By.id("long-value"));
+
+        String location = mapsBaseURL + "?q=" + latValue.getText() + "," + longValue.getText();
+
+        assertTrue(latValue.isDisplayed());
+
+        WebElement mapsLink = driver.findElement(By.xpath("//a[contains(text(), 'See it on Google')]"));
+
+        assertEquals(location, mapsLink.getAttribute("href"));
+    }
+
+    @After
+    public void Teardown()
+    {
+        driver.quit();
+    }
+}
