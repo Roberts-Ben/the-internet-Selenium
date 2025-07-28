31f34df077914ff285eb62bc402da94bedfb6b96
Ben Roberts <roberts.b93@gmail.com>
2025-07-28 16:11:17 +0100

    More tests
    - Dynamic Loading
    - Frames (iFrame)
    - Nested Frames

---
+++ src/test/java/DynamicLoading_Test.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DynamicLoading_Test
+{
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setup() throws Exception
+    {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/dynamic_loading", URL);
+    }
+
+    @Test
+    public void verifyHiddenElement()
+    {
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+
+        WebElement startButton = driver.findElement(By.xpath("//button"));
+        WebElement hiddenElement = driver.findElement(By.id("finish"));
+
+        assertFalse(hiddenElement.isDisplayed());
+
+        startButton.click();
+
+        wait.until(ExpectedConditions.visibilityOf(hiddenElement));
+        assertTrue(hiddenElement.isDisplayed());
+    }
+
+    @Test
+    public void verifyElementExistsAfterLoad()
+    {
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
+
+        WebElement startButton = driver.findElement(By.xpath("//button"));
+        List<WebElement> hiddenElement = driver.findElements(By.id("finish"));
+
+        assertEquals(0, hiddenElement.size());
+
+        startButton.click();
+
+        WebElement finalElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
+        assertTrue(finalElement.isDisplayed());
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
---
+++ src/test/java/Frames_Test.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Frames_Test
+{
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setup() throws Exception
+    {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/frames");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/frames", URL);
+    }
+
+    @Test
+    public void verifyNestedFramesLink()
+    {
+        WebElement nestedFramesLink = driver.findElement(By.xpath("//a[@href='/nested_frames']"));
+        nestedFramesLink.click();
+
+        assertEquals("https://the-internet.herokuapp.com/nested_frames", driver.getCurrentUrl());
+    }
+
+    @Test
+    public void verifyiFrame()
+    {
+        driver.get("https://the-internet.herokuapp.com/iframe");
+
+        // Need to close the TinyMCE read-only warning before interacting with other elements
+        WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button//div[@aria-label='Close']/..")));
+        closeButton.click();
+
+        WebElement iframe = driver.findElement(By.id("mce_0_ifr"));
+        driver.switchTo().frame("mce_0_ifr");
+
+        WebElement editor = driver.findElement(By.tagName("body"));
+
+        assertEquals("Your content goes here.", editor.getText());
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
