bb0bedb16ee3b4e9c0f80ecec030b5b34c523d71
Ben Roberts <roberts.b93@gmail.com>
2025-08-03 18:00:18 +0100

    More tests - Shifting Content, Slow Resources & TinyMCE

---
+++ src/test/java/ShiftingContent_Test.java
@@ -0,0 +1,129 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShiftingContent_Test extends BaseTest
+{
+    JavascriptExecutor js;
+    String baseURL;
+
+    @BeforeEach
+    public void setup() throws Exception
+    {
+        driver.get("https://the-internet.herokuapp.com/shifting_content");
+
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/shifting_content", URL);
+
+        baseURL = "https://the-internet.herokuapp.com";
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void verifyMenuElement()
+    {
+        WebElement menuLink = driver.findElement(By.xpath("//a[@href='/shifting_content/menu']"));
+
+        menuLink.click();
+        assertEquals("https://the-internet.herokuapp.com/shifting_content/menu", driver.getCurrentUrl());
+
+        ConfirmButtonElements();
+
+        WebElement shiftElementButton = driver.findElement(By.xpath("//a[@href='/shifting_content/menu?pixel_shift=100']"));
+
+        shiftElementButton.click();
+
+        ConfirmButtonElements();
+    }
+
+    @Test
+    public void verifyImageElement()
+    {
+        WebElement imageLink = driver.findElement(By.xpath("//a[@href='/shifting_content/image']"));
+
+        imageLink.click();
+        assertEquals("https://the-internet.herokuapp.com/shifting_content/image", driver.getCurrentUrl());
+
+        ConfirmImageElement();
+
+        WebElement shiftElementButton = driver.findElement(By.xpath("//a[@href='/shifting_content/image?pixel_shift=100']"));
+
+        shiftElementButton.click();
+
+        ConfirmImageElement();
+    }
+
+    @Test
+    public void verifyListElement()
+    {
+        WebElement listLink = driver.findElement(By.xpath("//a[@href='/shifting_content/list']"));
+
+        listLink.click();
+        assertEquals("https://the-internet.herokuapp.com/shifting_content/list", driver.getCurrentUrl());
+
+        ConfirmListElement();
+
+        driver.navigate().refresh();
+
+        ConfirmListElement();
+    }
+
+    private void ConfirmButtonElements()
+    {
+        WebElement listRoot = driver.findElement(By.tagName("ul"));
+        List<WebElement> buttons = listRoot.findElements(By.xpath(".//a[@href]"));
+
+        for(WebElement button : buttons)
+        {
+            String newURL = button.getAttribute("href");
+            js.executeScript("window.open()");
+
+            Object[] windowHandles = driver.getWindowHandles().toArray();
+            driver.switchTo().window((String) windowHandles[1]);
+
+            driver.get(newURL);
+            assertEquals(newURL, driver.getCurrentUrl());
+
+            driver.close();
+
+            driver.switchTo().window((String) windowHandles[0]);
+        }
+    }
+
+    private void ConfirmImageElement()
+    {
+        WebElement image = driver.findElement(By.className("shift"));
+
+        assertTrue(image.isDisplayed());
+    }
+
+    private void ConfirmListElement()
+    {
+        WebElement contentDiv = driver.findElement(By.cssSelector("div.large-6.columns.large-centered"));
+        String innerHTML = contentDiv.getAttribute("innerHTML");
+
+        String[] parts = innerHTML.split("<br\\s*/?>\\s*");
+        List<String> lines = Arrays.stream(parts).map(String::trim).filter(s -> !s.isEmpty()).toList();
+        String expectedString = "Important Information You're Looking For";
+
+        boolean stringFound = false;
+
+        for (String line : lines)
+        {
+            if(line.equals(expectedString))
+            {
+                stringFound = true;
+                break;
+            }
+        }
+
+        assertTrue(stringFound);
+    }
+}
---
+++ src/test/java/SlowResources_Test.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// NOTE: Currently this page fails with a 503 rather than loading the expected elements
+// To test around this:
+// Navigate directly to the page that calls the GET request and validating that it fails after 30 seconds instead of loading the content
+public class SlowResources_Test extends BaseTest
+{
+    @BeforeEach
+    public void setup() throws Exception
+    {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        driver.get("https://the-internet.herokuapp.com/slow");
+
+        String URL = driver.getCurrentUrl();
+        assertEquals("https://the-internet.herokuapp.com/slow", URL);
+    }
+
+    @Test
+    public void verifySlowElement()
+    {
+        int status = RestAssured.get("https://the-internet.herokuapp.com/slow_external").getStatusCode();
+
+        assertEquals(503, status);
+    }
+}
